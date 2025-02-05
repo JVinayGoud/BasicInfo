b1f3ececd4627906355004410af350caeaa71c62
JVinayGoud <vinaygoudj3@gmail.com>
2025-02-05 15:50:55 -0500

    Add files via upload

---
+++ script.js
@@ -0,0 +1,5 @@
+// Add any interactivity you want here (like smooth scroll, animations, etc.)
+
+document.addEventListener('DOMContentLoaded', () => {
+    console.log('Page loaded! You can add interactive functionality here.');
+});
